ae437a95136fe1a20579f2d0396564b5e7e47aa0
se-leo-rafael <leorafael.gaming@gmail.com>
2025-04-07 14:00:54 +0800

    Notifications can be sent outside, but not inside component code

--- app/customer/dashboard/book/page.tsx
+++ app/customer/dashboard/book/page.tsx
@@ -1,5 +1,25 @@
 import { BookingContent } from "@/components/customer-components/dashboard/book/booking-content"
 
+import notificationapi from 'notificationapi-node-server-sdk'
+
+notificationapi.init(
+  'owvp6sijxsgcijmqlu69gzfgcs', // If using .env, replace this with your clientId env variable
+  'a0w2yydeotd8e5p4u9qp9i4g4y6p38lzryla30i7b4e7nrr1r21590efl5', // If using .env, replace this with your clientSecret env variable
+)
+
+notificationapi.send({
+  notificationId: 'new_booking',
+  user: {
+    id: "leorafael.macaya.cics@ust.edu.ph",
+    email: "leorafael.macaya.cics@ust.edu.ph",
+    number: "+15005550006" // Replace with your phone number, use format [+][country code][area code][local number]
+  },
+  mergeTags: {
+    "comment": "testComment",
+    "commentId": "testCommentId"
+  }
+})
+
 export default function BookPage() {
   return (
     <div className="p-6">
