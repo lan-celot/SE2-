734973af5c43cc58e7f4047b0125b7c2c84475ba
Andrea Louise Salazar <andrealouise.salazar.cics@ust.edu.ph>
2025-04-11 03:36:32 +0800

    encrypted firebase keys

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,20 +1,21 @@
 import { initializeApp } from "firebase/app"
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
-import { getStorage } from "firebase/storage";
+import { getStorage } from "firebase/storage"
+
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyBoTY0dw2X3vvNNN36ajHvZpO926eHCebY",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "autotrack-3982e.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "autotrack-3982e",
-  storageBucket: "autotrack-3982e.firebasestorage.app",
-  messagingSenderId: "197356954496",
-  appId: "1:197356954496:web:9df3e88b7bb6eba35b9765",
-  measurementId: "G-BLJNBPYQ3N",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || ""
 }
 
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
-export const storage = getStorage(app);
+export const storage = getStorage(app)
 export const db = getFirestore(app)
 
 export const loginUser = async (email: string, password: string) => {
@@ -28,11 +29,9 @@ export const loginUser = async (email: string, password: string) => {
     }
   } catch (error) {
     console.error("Firebase login error:", error)
-    // Return an error object instead of throwing
     return {
       success: false,
       error: "Invalid credentials",
     }
   }
 }
-
