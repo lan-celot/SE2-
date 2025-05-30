9cd9a62ffd9974b576fed0960dab4e15783a6144
lan-celot <liamlancelot.vitug.cics@ust.edu.ph>
2025-05-30 15:51:48 +0800

    deployment fix

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('postcss-load-config').Config} */
-export default {
+const config = {
   plugins: {
-    'postcss-import': {},
-    'tailwindcss/nesting': {},
     tailwindcss: {},
     autoprefixer: {},
   },
 };
+
+export default config;
 
